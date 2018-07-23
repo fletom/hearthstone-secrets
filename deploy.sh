df7794e8b3db97a07c45b3eef109bc2934eef0ec
Fletcher Tomalty <fletcher@tomalty.com>
2018-07-22 20:59:51 -0400

    add CloudFront invalidation to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 set -euf -o pipefail
 
 S3_BUCKET_NAME='hearthstone-secrets.fletom.com'
+CLOUDFRONT_DISTRIBUTION_ID='E14DPMMLGM0NE4'
 EXCLUDE=('.DS_Store' 'deploy.sh' '.git/*' '.idea/*')
 
 exclude_params=''
 for e in ${EXCLUDE[@]}; do
 	exclude_params="$exclude_params --exclude $e"
 done
+
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
 
 aws s3 sync \
 	./ s3://$S3_BUCKET_NAME \
